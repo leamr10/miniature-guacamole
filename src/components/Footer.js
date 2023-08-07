5d379b288fa342644711b7b9944ffe4dc7540c2f
leamr10 <leamr10@gmail.com>
2023-08-07 14:19:16 -0400

    footer edited

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container-text-center">
+                <h3>GitHub: </h3><a href="https://github.com/leamr10"></a>
+                <h3> Linkedin:</h3><a href="https://www.linkedin.com/in/mitchell-lea-794712126/"></a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
