6ebff00e5400bf0ba0ae76d9912fe371b4931452
leamr10 <leamr10@gmail.com>
2023-08-07 15:03:38 -0400

    about edited

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4">
+                    <img src="./images/mitchellpicture.png" alt="Picture of Mitchell Lea"/>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = ({currentPage, handlePageChange}) => {
+    const sections = [
+        {name: "About Me", link: "#about"},
+        {name: "Portfolio", link: "#portfolio"},
+        {name: "Resume", link: "#resume"},
+        {name: "Contact", link: "#contact"},
+    ];
+
+    return (
+
+    );
+};
+
+export default Navigation;
